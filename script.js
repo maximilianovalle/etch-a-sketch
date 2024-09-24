ebe16c4dda78d009fe39c3f340606093b0a6d00b
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2024-09-23 22:36:27 -0500

    Allow grid size changes and canvas reset

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-let divHeightWidth = 16;
-let divPercentage = 100/16;
-
-function setNewDivPercentage() {
-    divPercentage = 100/divHeightWidth;
-}
+let divHeightWidth = 16; // initial canvas 16x16
+let divPercentage = 100/divHeightWidth;
 
 function setNewDivHeightWidth(userInput) {
     divHeightWidth = userInput;
 }
 
-// grid functions
+function setNewDivPercentage() {
+    divPercentage = 100/divHeightWidth;
+}
 
+// grid functions -----
 
-function createGrid() { // when page first loads
+function createGrid() { // automatically called when page first loads
     const divContainer = getContainer();    // #etchSketchScreen
 
     for (let row = 0; row < divHeightWidth; row++) {
@@ -30,8 +29,23 @@ function createGrid() { // when page first loads
             currRow.appendChild(divColumn);
         }
     }
-
 }
+
+function clearGrid() {
+    const divRowContainers = getRowContainer();
+    const divColumnContainers = getColumnContainer();
+
+    for (let i = 0; i < divColumnContainers.length; i++) {
+        let currColumn = divColumnContainers[i];
+        currColumn.remove();
+    }
+
+    for (let i = 0; i < divRowContainers.length; i++) {
+        let currRow = divRowContainers[i];
+        currRow.remove();
+    }
+}
+
 
 function getContainer() {
     return document.querySelector("#etchSketchScreen");
@@ -39,6 +53,10 @@ function getContainer() {
 
 function getRowContainer() {
     return document.querySelectorAll(".row");
+}
+
+function getColumnContainer() {
+    return document.querySelectorAll(".column");
 }
 
 function createDivRow() {
@@ -56,21 +74,64 @@ function createDivColumn() {
     return column;
 }
 
-// button functions + code
+// WIP -- figure out black pen coloring on column divs when onmouseover?? onmousedown???
 
-function showTestAlert() {
-    alert("testing");
-}
+
+// button functions + code -----
 
 function getButton(buttonID) {
     return document.querySelector("#" + buttonID);
 }
 
+
 const gridButton = getButton("gridButt");
-gridButton.addEventListener("click", showTestAlert);
+gridButton.addEventListener("click", promptGridButton);
+
+function promptGridButton() {
+    let newInput = prompt("What grid size would you like? Insert num from 1-80!");  // WIP -- REFINE POPUP
+
+    if (newInput > 0 && newInput < 81) {
+        setNewDivHeightWidth(newInput);
+        setNewDivPercentage();
+        clearGrid();
+        createGrid();
+        return;
+    }
+
+    else if (newInput < 1 || newInput > 80) {
+        alert("Number not within 1-80 range! Try again.");
+        return;
+    }
+
+    alert("Invalid input! Try again...");
+}
+
 
 const rainbowButton = getButton("rainbowButt");
-rainbowButton.addEventListener("click", showTestAlert);
+rainbowButton.addEventListener("click", promptRainbowButton);
 
-const resetButt = getButton("resetButt");
-resetButt.addEventListener("click", showTestAlert);
+function promptRainbowButton() { // WIP
+    // figure out regular black pen color function first
+}
+
+
+const clearButton = getButton("clearButt");
+clearButton.addEventListener("click", promptClearButton);
+
+function promptClearButton() {
+    let resetCanvas = prompt("Are you sure you want to clear? Insert 'Yes' or 'No'");    // WIP -- REFINE POPUP
+
+    if (resetCanvas == "Yes" || resetCanvas == "yes" || resetCanvas == "y" || resetCanvas == "Y") {
+        alert("canvas reset.");
+        clearGrid();
+        createGrid();
+        return;
+    }
+
+    if (resetCanvas == "No" || resetCanvas == "no" || resetCanvas == "n" || resetCanvas == "N") {
+        alert("exited.");
+        return;
+    }
+
+    alert("Invalid input. Try again...");
+}
